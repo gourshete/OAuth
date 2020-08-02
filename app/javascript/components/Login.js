bf88eb2093c4a2fd9764045eee96a0b4d9bb8e21
gourshete <swapnilggourshete@gmail.com>
2020-08-03 00:21:38 +0530

    - add HelloWorld component - add devise model User - expand devise:user views

---
+++ app/javascript/components/Login.js
@@ -0,0 +1,12 @@
+function Login(props){
+    return(
+        <div>
+            Please Login to continue
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <Login />,
+    document.getElementById('root')
+  );
